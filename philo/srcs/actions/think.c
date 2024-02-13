02e3c9090bf632533db7f87c9e3aa973a311a70e
Jimmy DENIS <jdenis@bess-f1r5s11.clusters.42paris.fr>
2024-02-13 18:19:36 +0100

    correction double eat

--- philo/srcs/actions/think.c
+++ philo/srcs/actions/think.c
@@ -6,7 +6,7 @@
 /*   By: jdenis <jdenis@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/02 15:42:00 by jdenis            #+#    #+#             */
-/*   Updated: 2024/02/12 19:02:05 by jdenis           ###   ########.fr       */
+/*   Updated: 2024/02/13 18:18:25 by jdenis           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,6 @@
 void	think(t_philo *philo)
 {
 	print_lock("is thinking", philo);
-	// while (get_current_time() - philo->last_meal < 0.9 * philo->time_to_die)
-	// 	ft_usleep(1, philo);
+	while (get_current_time() - philo->last_meal < 0.9 * philo->time_to_die)
+		ft_usleep(1, philo);
 }
