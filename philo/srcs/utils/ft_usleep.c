55e3381b0bf7eccec6427ea98a6a51c702cf0939
Jimmy DENIS <jdenis@bess-f1r5s11.clusters.42paris.fr>
2024-02-13 18:15:24 +0100

    correction usleep

--- philo/srcs/utils/ft_usleep.c
+++ philo/srcs/utils/ft_usleep.c
@@ -6,7 +6,7 @@
 /*   By: jdenis <jdenis@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/29 17:37:47 by jdenis            #+#    #+#             */
-/*   Updated: 2024/02/08 10:08:41 by jdenis           ###   ########.fr       */
+/*   Updated: 2024/02/12 19:34:00 by jdenis           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,7 +27,13 @@ int	ft_usleep(size_t time, t_philo *philo)
 
 	(void)philo;
 	start = get_current_time();
-	while (get_current_time() < start + time)
+	pthread_mutex_lock(philo->dead_lock);
+	while (get_current_time() < start + time && !*philo->dead)
+	{
+		pthread_mutex_unlock(philo->dead_lock);
 		usleep(10);
+		pthread_mutex_lock(philo->dead_lock);
+	}
+	pthread_mutex_unlock(philo->dead_lock);
 	return (0);
 }
